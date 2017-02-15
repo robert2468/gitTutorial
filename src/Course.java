70cb62d266f9ff7ada260750607291e14fbdc3a1
Robert Park <treborkrap2468@gmail.com>
2017-02-14 19:04:37 -0500

    Merged feature1 and feature2

---
+++ src/Course.java
@@ -0,0 +1,17 @@
+/**
+ * Created by rober on 2/14/2017.
+ */
+public class Course {
+
+    String cname;
+    String description;
+
+    public Course (String cname, String description){
+
+        this.cname = cname;
+        this.description = description;
+
+    }
+
+
+}
